bda58d257878263f139d8d9053a9f958904bde1c
Iron Man <pangdongcan@live.com>
2024-11-29 17:20:52 +0800

    1.0.9-SNAPSHOT: improve original functions and add mass sending function.

--- src/main/java/net/mickarea/tools/utils/EmailUtil.java
+++ src/main/java/net/mickarea/tools/utils/EmailUtil.java
@@ -33,10 +33,10 @@ import javax.mail.internet.MimeUtility;
 import net.mickarea.tools.utils.security.RSAUtil;
 
 /**
- * &gt;&gt;&nbsp;关于电子邮件的收发处理
+ * 关于电子邮件的收发处理
  * @author Michael Pang (Dongcan Pang)
  * @version 1.0
- * @since 2023年9月27日-2024年4月3日
+ * @since 2023年9月27日-2024年11月29日
  */
 public final class EmailUtil {
 	
@@ -54,7 +54,22 @@ public final class EmailUtil {
 	private static final Map<String, String> EMAIL_CONFIG = emailConfigInit();
 	
 	/**
-	 * &gt;&gt;&nbsp;邮件配置信息对象，初始化
+	 * 默认的发送方名称。
+	 */
+	public static final String DEFAULT_FROM_NAME = "mickarea.net 官方服务";
+	
+	/**
+	 * 默认的主题信息
+	 */
+	public static final String DEFAULT_SUBJECT = "无主题";
+	
+	/**
+	 * 默认的字符集
+	 */
+	public static final String DEFAULT_CHARSET = "UTF-8";
+	
+	/**
+	 * 邮件配置信息对象，初始化
 	 * @return 配置信息 map
 	 */
 	private static Map<String, String> emailConfigInit(){
@@ -146,16 +161,44 @@ public final class EmailUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;邮件一对一发送方法
+	 * 邮件一对一发送方法
 	 * @param subject 邮件主题信息
 	 * @param htmlString 要发送的内容，html 格式
 	 * @param toEmailAddr 目标邮件地址
 	 * @param charsetName 邮件内容字符集 。比如 GBK, GB18030, UTF-8 之类的
+	 * @return 如果发送成功，返回 true ；如果发送失败，返回 false 。
 	 */
-	public static void sendEmail(String subject, String htmlString, String toEmailAddr, String charsetName) {
-		
+	public static boolean sendEmail(String subject, String htmlString, String toEmailAddr, String charsetName) {
+		return sendEmail(DEFAULT_FROM_NAME, subject, htmlString, toEmailAddr, charsetName);
+	}
+	
+	/**
+	 * 邮件一对一发送方法（邮件内容字符集 默认 GBK）
+	 * @param subject 邮件主题信息
+	 * @param htmlString 要发送的内容，html 格式
+	 * @param toEmailAddr 目标邮件地址
+	 * @return 如果发送成功，返回 true ；如果发送失败，返回 false 。
+	 */
+	public static boolean sendEmail(String subject, String htmlString, String toEmailAddr) {
+		return sendEmail(subject, htmlString, toEmailAddr, "GBK");
+	}
+	
+	/**
+	 * 邮件一对一发送方法
+	 * @param fromName 发送方的名称。不是邮件地址，是一个简单的名称
+	 * @param subject 邮件主题信息
+	 * @param htmlString 要发送的内容，html 格式
+	 * @param toEmailAddr 目标邮件地址
+	 * @param charsetName 邮件内容字符集 。比如 GBK, GB18030, UTF-8 之类的
+	 * @return 如果发送成功，返回 true ；如果发送失败，返回 false 。
+	 */
+	public static boolean sendEmail(String fromName, String subject, String htmlString, String toEmailAddr, String charsetName) {
+		// 定义一个返回执行结果的变量
+		boolean result = false;
+		// 获取服务器的账号和密码
 		String serverUserName = EmailUtil.EMAIL_CONFIG.get("serverUserName");
 		String serverPassword = EmailUtil.EMAIL_CONFIG.get("serverPassword");
+		// 
 		if(StrUtil.isEmptyString(serverUserName) || StrUtil.isEmptyString(serverPassword)) {
 			Stdout.pl("邮件服务的账号或者密码缺失，请检查配置信息");
 		}else {
@@ -169,7 +212,7 @@ public final class EmailUtil {
 					}
 				});
 				//Stdout.pl(props);
-				//
+				// 处理服务器的账户验证
 				Session session = Session.getInstance(props, new Authenticator() {
 					@Override
 					protected PasswordAuthentication getPasswordAuthentication() {
@@ -177,18 +220,23 @@ public final class EmailUtil {
 					}
 				});
 				//处理邮件发送方的名称，如果转码错误，则直接显示 发送方邮箱地址
-				String fromName = null;
+				String senderName = null;
 				try {
-					fromName = MimeUtility.encodeWord("mickarea.net 官方服务") + "<" + serverUserName + ">" ;
+					senderName = MimeUtility.encodeWord(fromName) + "<" + serverUserName + ">" ;
 				}catch(Exception e) {
-					fromName = serverUserName;
+					senderName = serverUserName;
 				}
 				//邮件信息对象
 				Message message = new MimeMessage(session);
 				try {
-					message.setFrom(new InternetAddress(fromName));
+					message.setFrom(new InternetAddress(senderName));
 					message.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmailAddr));
-					message.setSubject(StrUtil.isEmptyString(subject)?"无主题":subject);
+					message.setSubject(StrUtil.isEmptyString(subject)?DEFAULT_SUBJECT:subject);
+					// 校验字符集是否正确
+					if(!StrUtil.isCharsetSupported(charsetName)) {
+						throw new Exception("字符集 "+charsetName+"，不被支持，请检查输入是否正确");
+					}
+					//
 					message.setContent(htmlString, "text/html;charset="+charsetName);
 					message.setSentDate(new Date());
 					message.saveChanges();
@@ -200,7 +248,11 @@ public final class EmailUtil {
 				//发送邮件
 				try {
 					Transport.send(message);
+					//如果发送成功，返回 true 值。
+					result = true;
+					//
 					Stdout.pl("发送邮件到对方邮箱("+toEmailAddr+")成功");
+					//
 				}catch(SendFailedException e1) { 
 					Stdout.pl("发送邮件到对方邮箱("+toEmailAddr+")失败1, "+e1.getMessage());
 					Stdout.pl(e1);
@@ -215,17 +267,132 @@ public final class EmailUtil {
 				Stdout.pl("传入的参数无效, htmlString("+htmlString+"), toEmailAddr("+toEmailAddr+")");
 			}
 		}
-		
+		return result;
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;邮件一对一发送方法（邮件内容字符集 默认 GBK）
+	 * 群发邮件的处理方法
+	 * @param fromName 发送方的名称。不是邮件地址，是一个简单的名称
 	 * @param subject 邮件主题信息
 	 * @param htmlString 要发送的内容，html 格式
-	 * @param toEmailAddr 目标邮件地址
+	 * @param toEmailAddrs 待发送的邮件地址列表
+	 * @param charsetName 邮件内容字符集 。比如 GBK, GB18030, UTF-8 之类的
+	 * @return 如果发送成功，返回 true ；如果发送失败，返回 false 。
 	 */
-	public static void sendEmail(String subject, String htmlString, String toEmailAddr) {
-		sendEmail(subject, htmlString, toEmailAddr, "GBK");
+	public static boolean sendMassEmail(String fromName, String subject, String htmlString, List<String> toEmailAddrs, String charsetName) {
+		// 定义一个返回执行结果的变量
+		boolean result = false;
+		// 获取服务器的账号和密码
+		String serverUserName = EmailUtil.EMAIL_CONFIG.get("serverUserName");
+		String serverPassword = EmailUtil.EMAIL_CONFIG.get("serverPassword");
+		// 
+		if(StrUtil.isEmptyString(serverUserName) || StrUtil.isEmptyString(serverPassword)) {
+			Stdout.pl("邮件服务的账号或者密码缺失，请检查配置信息");
+		}else {
+			if(!StrUtil.isEmptyString(htmlString) && !ListUtil.isEmptyList(toEmailAddrs)) {
+				//如果传入的参数有效才执行
+				Properties props = new Properties();
+				EmailUtil.EMAIL_CONFIG.forEach((key,value)->{
+					// mail. 开头的参数才加入到 props
+					if(Pattern.matches("^mail\\..+", key)) {
+						props.put(key, value);
+					}
+				});
+				//Stdout.pl(props);
+				// 处理服务器的账户验证
+				Session session = Session.getInstance(props, new Authenticator() {
+					@Override
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(serverUserName, serverPassword);
+					}
+				});
+				//处理邮件发送方的名称，如果转码错误，则直接显示 发送方邮箱地址
+				String senderName = null;
+				try {
+					senderName = MimeUtility.encodeWord(fromName) + "<" + serverUserName + ">" ;
+				}catch(Exception e) {
+					senderName = serverUserName;
+				}
+				//邮件信息对象
+				Message message = new MimeMessage(session);
+				try {
+					message.setFrom(new InternetAddress(senderName));
+					//
+					// message.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmailAddr));
+					// 群发处理
+					InternetAddress[] addrs = new InternetAddress[toEmailAddrs.size()];
+					for(int i=0;i<addrs.length;i++) {
+						addrs[i] = new InternetAddress(toEmailAddrs.get(i));
+					}
+					message.setRecipients(Message.RecipientType.TO, addrs);
+					//
+					message.setSubject(StrUtil.isEmptyString(subject)?DEFAULT_SUBJECT:subject);
+					// 校验字符集是否正确
+					if(!StrUtil.isCharsetSupported(charsetName)) {
+						throw new Exception("字符集 "+charsetName+"，不被支持，请检查输入是否正确");
+					}
+					//
+					message.setContent(htmlString, "text/html;charset="+charsetName);
+					message.setSentDate(new Date());
+					message.saveChanges();
+				}catch(Exception e) {
+					message = null;
+					Stdout.pl("构造邮件 Message 对象出错");
+					Stdout.pl(e);
+				}
+				//发送邮件
+				try {
+					Transport.send(message);
+					//如果发送成功，返回 true 值。
+					result = true;
+					//
+					Stdout.pl("发送邮件到对方邮箱成功");
+					Stdout.pl(toEmailAddrs);
+					//
+				}catch(SendFailedException e1) { 
+					Stdout.pl("发送邮件到对方邮箱失败1, "+e1.getMessage());
+					Stdout.pl(toEmailAddrs);
+					Stdout.pl(e1);
+				}catch (MessagingException e2) {
+					Stdout.pl("发送邮件到对方邮箱失败2, "+e2.getMessage());
+					Stdout.pl(toEmailAddrs);
+					Stdout.pl(e2);
+				}catch(Exception e3) {
+					Stdout.pl("发送邮件到对方邮箱失败3, "+e3.getMessage());
+					Stdout.pl(toEmailAddrs);
+					Stdout.pl(e3);
+				}
+			}else {
+				Stdout.pl("传入的参数无效, htmlString("+htmlString+")");
+				Stdout.pl("传入的参数无效");
+				Stdout.pl(toEmailAddrs);
+			}
+		}
+		return result;
 	}
+	
+	//测试函数
+	/*
+	public static void main(String[] args) {
+		// 方法1
+		//Stdout.pl("方法1："+sendEmail("邮件发送测试 方法1", "<h2>请勿回复 方法1</h2>", "pangdongcan@live.com"));
+		// 方法2
+		//Stdout.pl("方法2："+sendEmail("邮件发送测试 方法2", "<h2>请勿回复 方法2</h2>", "pangdongcan@live.com", "UTF-8"));
+		// 方法3
+		//Stdout.pl("方法3："
+		//+sendEmail("Micheal Pang 测试", "邮件发送测试 方法3", "<h2>请勿回复 方法3</h2>", "pangdongcan@live.com", "UTF-8"));
+		//
+		//List<String> addrs = Arrays.asList("pangdongcan@live.com", "mickarea.net@gmail.com");
+		// 方法4
+		//Stdout.pl("方法4："
+		//+sendMassEmail("Pang Dong Can 测试", "邮件发送测试（群发） 方法4", "<h2>请勿回复 方法4</h2>", addrs, "UTF-8"));
+		//
+		//Stdout.pl("方法 5："+sendEmail(null, null, "<h2>测试5</h2>", "pangdongcan@live.com", "UTF-8"));
+		//
+		//List<String> addrs = Arrays.asList("xxx@live.com", "yyy@gmail.com");
+		//Stdout.pl("方法6："
+		//+sendMassEmail(null, null, "<h2>测试6</h2>", addrs, "UTF-8"));
+	}
+	*/
 	
 }

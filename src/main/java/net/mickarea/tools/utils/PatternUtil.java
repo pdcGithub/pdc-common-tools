954f48beed8b5341798e160cf4b0aa2d7c7be485
Iron Man <pangdongcan@live.com>
2024-12-02 16:40:15 +0800

    1.0.9-SNAPSHOT: add function 'genRegStrForHtmlTagReplace'

--- src/main/java/net/mickarea/tools/utils/PatternUtil.java
+++ src/main/java/net/mickarea/tools/utils/PatternUtil.java
@@ -16,10 +16,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * &gt;&gt;&nbsp;一个正则工具类
+ * 一个正则工具类
  * @author Michael Pang (Dongcan Pang)
  * @version 1.0
- * @since 2023年4月24日-2023年11月13日
+ * @since 2023年4月24日-2024年12月2日
  */
 public final class PatternUtil {
 	
@@ -56,7 +56,7 @@ public final class PatternUtil {
 	public static final String UPLOAD_FILE_NAME = "filename=\"(.*)\"";
 	
 	/**
-	 * &gt;&gt;&nbsp;提取上传的文件头报文
+	 * 提取上传的文件头报文
 	 * @param str 源头字符串
 	 * @return 目标字符串
 	 */
@@ -72,7 +72,7 @@ public final class PatternUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;提取文件的真实名称
+	 * 提取文件的真实名称
 	 * @param str 带有名称的报文头部
 	 * @return 文件的真实名称
 	 */
@@ -92,7 +92,7 @@ public final class PatternUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;提取文件名中的后缀
+	 * 提取文件名中的后缀
 	 * @param str 一个文件名
 	 * @return 后缀字符串
 	 */
@@ -108,7 +108,7 @@ public final class PatternUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;提取上传的文件末尾
+	 * 提取上传的文件末尾
 	 * @param str 报文
 	 * @return 末尾字符串
 	 */
@@ -124,7 +124,7 @@ public final class PatternUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;校验传入的邮件地址是否符合邮件地址规范
+	 * 校验传入的邮件地址是否符合邮件地址规范
 	 * @param address 将要校验的邮件地址信息
 	 * @return 如果符合，则返回true；否则，返回false
 	 */
@@ -137,7 +137,7 @@ public final class PatternUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;利用正则表达式，从目标字符串提取对应的内容
+	 * 利用正则表达式，从目标字符串提取对应的内容
 	 * @param patternStr 提取字符串用的正则表达式
 	 * @param oriStr 被提取的目标字符串
 	 * @return 提取出来的结果列表；如果正则表达式报错，则返回null; 如果正常执行，则返回一个 List 对象
@@ -166,7 +166,7 @@ public final class PatternUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;从html文本中，查找纯文本内容。
+	 * 从html文本中，查找纯文本内容。
 	 * <p>比如：&lt;p style='text-align:center;'&gt;测试&lt;/p&gt;内容，返回值为：测试</p>
 	 * @param htmlContent 待处理的html文本
 	 * @return 纯文本内容
@@ -179,14 +179,49 @@ public final class PatternUtil {
 		return result;
 	}
 	
+	/**
+	 * 生成一个正则字符串，用于html标签文本整体替换。如果传入的是一个空字符串 或者 null，则返回 空字符串。
+	 * @param tagName html 标签名，大小写都可以，因为内部会自动转换
+	 * @return 一个正则字符串
+	 */
+	public static String genRegStrForHtmlTagReplace(String tagName) {
+		String re = "";
+		// 不为空才处理
+		if(!StrUtil.isEmptyString(tagName)) {
+			// 转小写字母
+			String lowerCaseStr = tagName.toLowerCase();
+			// 开始 生成一个名字字符串 
+			StringBuffer tagNameSb = new StringBuffer();
+			for(char c: lowerCaseStr.toCharArray()) {
+				tagNameSb.append("[");
+				if( c >= 97 && c <= 122 ) { // 小写字母 a-z 的范围
+					tagNameSb.append((char)(c-32)); // 添加 大写字母 内容
+				}
+				tagNameSb.append(c);
+				tagNameSb.append("]");
+			}
+			String tmpName = tagNameSb.toString();
+			//
+			re = "[\\s]*<("+tmpName+"\\s+.*?|"+tmpName+")>[\\s\\S]*?</("+tmpName+")>[\\s]*";
+		}
+		return re;
+	}
+	
 	//测试函数
 	/*
 	public static void main(String[] args) {
-		Stdout.pl(isEmailAddress("674256821@qq.com"));
-		Stdout.pl(isEmailAddress("sdlfis.d6f87d.s5-sdfd-s5_asdfs@163.com"));
-		Stdout.pl(isEmailAddress("net.mick_area@gmail.com"));
-		Stdout.pl(isEmailAddress("mick123.com.123.com@...sd5fsd4f5dsf"));
-		Stdout.pl(isEmailAddress("mick123.com.123.com@dfsfdf@...sd5fsd4f5dsf"));
+		String formStr = "[\\s]*<([Ff][Oo][Rr][Mm]\\s+.*?|[Ff][Oo][Rr][Mm])>[\\s\\S]*?</([Ff][Oo][Rr][Mm])>[\\s]*";
+		String newStr = genRegStrForHtmlTagReplace("Form2");
+		Stdout.pl(formStr);
+		Stdout.pl(newStr);
+		Stdout.pl(formStr.equals(newStr));
+		String scriptStr = "[\\s]*<([Ss][Cc][Rr][Ii][Pp][Tt]\\s+.*?|[Ss][Cc][Rr][Ii][Pp][Tt])>[\\s\\S]*?</([Ss][Cc][Rr][Ii][Pp][Tt])>[\\s]*";
+		String newStr2 = genRegStrForHtmlTagReplace("scRipt");
+		Stdout.pl(scriptStr);
+		Stdout.pl(newStr2);
+		Stdout.pl(scriptStr.equals(newStr2));
+		Stdout.pl(genRegStrForHtmlTagReplace("H2"));
+		Stdout.pl(genRegStrForHtmlTagReplace("objectZip"));
 	}
 	*/
 }

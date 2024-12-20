1af7c076bb3167d2c7bb8330668f604f0e306b29
Iron Man <pangdongcan@live.com>
2024-12-20 13:03:39 +0800

    1.0.10-SNAPSHOT: add 2 functions, 'getJavaBeanFieldsInfo' and 'getModifierInfo'.

--- src/main/java/net/mickarea/tools/utils/StrUtil.java
+++ src/main/java/net/mickarea/tools/utils/StrUtil.java
@@ -10,6 +10,8 @@ Copyright (c) 2022 Michael Pang.
 *******************************************************************************************************/
 package net.mickarea.tools.utils;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
@@ -19,10 +21,10 @@ import java.util.regex.Pattern;
 import net.mickarea.tools.utils.database.DBSQLInjectionUtil;
 
 /**
- * &gt;&gt;&nbsp;个人的一个字符工具类
+ * 个人的一个字符工具类
  * @author Michael Pang (Dongcan Pang)
  * @version 1.1
- * @since 2022年6月26日-2024年12月2日
+ * @since 2022年6月26日-2024年12月20日
  */
 public final class StrUtil {
 	
@@ -68,7 +70,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;根据输入的英文字符串，返回一个驼峰表示的字符串
+	 * 根据输入的英文字符串，返回一个驼峰表示的字符串
 	 * @param input 输入的字符串
 	 * @param splitString 字符串中的分割符号
 	 * @return 一个驼峰表示的字符串
@@ -89,7 +91,7 @@ public final class StrUtil {
 	
 	
 	/**
-	 * &gt;&gt;&nbsp;判断字符串参数是否为空，当字符内容为null也判断为空。
+	 * 判断字符串参数是否为空，当字符内容为null也判断为空。
 	 * @param param 待判断的字符串参数
 	 * @return 如果param为null，'null'，空白字符，空等则为true。
 	 */
@@ -102,7 +104,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;网页参数注入转换，用于防范网页攻击
+	 * 网页参数注入转换，用于防范网页攻击
 	 * @param param 待过滤字符
 	 * @return 转换完毕的字符
 	 */
@@ -113,7 +115,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;Sql语句参数注入转换，用于防范sql注入。（默认为：MYSQL）
+	 * Sql语句参数注入转换，用于防范sql注入。（默认为：MYSQL）
 	 * @param sqlParam sql语句的参数
 	 * @return 转换完毕的字符
 	 */
@@ -122,7 +124,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;Sql语句参数注入转换，用于防范sql注入。
+	 * Sql语句参数注入转换，用于防范sql注入。
 	 * @param dbType 要转义的数据库类型。参考：DBSQLInjectionUtil 类中的 <strong>DBTYPE</strong>常量
 	 * @param sqlParam sql语句的参数
 	 * @return 转换完毕的字符
@@ -132,7 +134,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;对于html的内容进行转义。否则，网页显示会有影响
+	 * 对于html的内容进行转义。否则，网页显示会有影响
 	 * @param htmlParam 待转换的内容
 	 * @return 转换后的内容
 	 */
@@ -146,7 +148,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;对于html的内容进行转义。否则，网页显示会有影响
+	 * 对于html的内容进行转义。否则，网页显示会有影响
 	 * <p>这里是关于富文本编辑器的内容转换问题。因为富文本无法直接转换 &lt; 和 &gt; 。所以，需要更加强大的防注入转换</p>
 	 * @param htmlParam 待转换的内容
 	 * @return 转换后的内容
@@ -172,7 +174,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;在原始字符串中，查找有多少个 匹配的字符串，返回匹配数量
+	 * 在原始字符串中，查找有多少个 匹配的字符串，返回匹配数量
 	 * @param ori 原始字符串
 	 * @param matchStr  匹配的字符串，使用正则表达式
 	 * @return 匹配数量
@@ -189,7 +191,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;判断输入的字符串是否与java关键字同名
+	 * 判断输入的字符串是否与java关键字同名
 	 * @param input 待判断的字符串
 	 * @return 如果与java关键字同名，则返回true；否则，返回false
 	 */
@@ -205,7 +207,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;判断输入的字符集的名称是否符合规范
+	 * 判断输入的字符集的名称是否符合规范
 	 * @param charsetName 字符集名称。比如：GBK, UTF-8, ISO-8859-1 等等
 	 * @return 如果字符集能识别，则返回 true；否则 false 。
 	 */
@@ -221,7 +223,7 @@ public final class StrUtil {
 	}
 	
 	/**
-	 * &gt;&gt;&nbsp;删除待处理字符串中的所有空白字符
+	 * 删除待处理字符串中的所有空白字符
 	 * @param words 待处理字符
 	 * @return 如果words参数为null，返回空字符串""。否则，返回处理后的字符串
 	 */
@@ -233,16 +235,108 @@ public final class StrUtil {
 		return result;
 	}
 	
+	/**
+	 * 获取 Java bean 类的属性信息字符串。通常是一个全部属性的拼接完整的内容。
+	 * @param targetObj 待处理对象
+	 * @return 属性信息字符串。如果参数为null，可能返回 null。
+	 */
+	public static final String getJavaBeanFieldsInfo(Object targetObj) {
+		
+		//如果传来的是null对象，那就直接返回 null
+		if(targetObj==null) {
+			return null;
+		}
+		
+		//定义返回的结果对象
+		StringBuffer sb = new StringBuffer();
+		
+		//获取该对象的所有属性
+		Field[] fields = targetObj.getClass().getDeclaredFields();
+		
+		//(拼接类名)
+		sb.append(targetObj.getClass().getSimpleName());
+		sb.append("{");
+		//拼接属性名称 和 数值
+		if(fields!=null && fields.length>0) {
+			for(int i=0;i<fields.length;i++) {
+				Field tmpField = fields[i];
+				String fieldName = tmpField.getName();
+				String fieldValue = null;
+				try {
+					//设置 可访问性，为 true，强制访问（有些属性是私有的无法直接访问，除非设置为可访问）
+					if(!tmpField.isAccessible()) {
+						tmpField.setAccessible(true);
+					}
+					//获取字符串形式的内容
+					fieldValue = tmpField.get(targetObj)==null?null:tmpField.get(targetObj).toString();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				// 添加 修饰符信息
+				sb.append(getModifierInfo(tmpField));
+				//拼接属性名和属性值
+				sb.append(fieldName);
+				sb.append("=");
+				sb.append(fieldValue);
+				if(i<fields.length-1) {
+					sb.append(", ");
+				}
+			}
+		}
+		sb.append("}");
+		
+		//返回结果
+		return sb.toString();
+	}
+	
+	/**
+	 * 根据传来的 Java 类的属性对象，解析它所定义的关键字是什么
+	 * @param f Java 类的属性对象
+	 * @return 修饰的关键字信息
+	 */
+	public static final String getModifierInfo(Field f) {
+		//定义返回的结果对象
+		StringBuffer sb = new StringBuffer();
+		//
+		if(f!=null) {
+			sb.append("(");
+			int modifiers = f.getModifiers();
+			if(Modifier.isPublic(modifiers)) {
+				sb.append("public ");
+			}
+			if(Modifier.isPrivate(modifiers)) {
+				sb.append("private ");
+			}
+			if(Modifier.isProtected(modifiers)) {
+				sb.append("protected ");
+			}
+			if(Modifier.isStatic(modifiers)) {
+				sb.append("static ");
+			}
+			if(Modifier.isFinal(modifiers)) {
+				sb.append("final ");
+			}
+			//这里把前内容输出，然后把最后一个空格去掉
+			String tmp = sb.toString().trim();
+			sb.setLength(0);
+			sb.append(tmp);//重新设置
+			sb.append(")");//最后的拼接
+		}else {
+			//如果传来的属性对象是null，则直接写 null
+			sb.append("(null)");
+		}
+		//返回结果
+		return sb.toString();
+	}
+	
 	/*
 	public static void main(String[] args) {
-		String ori = FileUtil.loadStringFromFile("C:\\Users\\Michael\\Desktop", "injection_test.html", true);
-		Stdout.pl(ori);
-		Stdout.pl("============================================");
-		String newStr = injectionTranslateForHtmlEditor(ori);
-		Stdout.pl(newStr);
-		Stdout.pl("===================== 写入到文件 ===================");
-		boolean isOk = FileUtil.saveToLocalpath(newStr, "C:\\Users\\Michael\\Desktop", "ok_test.html", "UTF-8");
-		Stdout.pl("文件写入 "+(isOk?"成功":"失败"));
+		TestA a1 = null;
+		TestA a2 = new TestA();
+		TestA a3 = new TestA(11, "tom", "男", "地址", "150174373");
+		Stdout.pl(getJavaBeanFieldsInfo(a1));
+		Stdout.pl(getJavaBeanFieldsInfo(a2));
+		Stdout.pl(getJavaBeanFieldsInfo(a3));
 	}
 	*/
 }

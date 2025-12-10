d1635dbd513edc64e2ca4a1ba9bd8c04d9e13fa1
Iron Man <pangdongcan@live.com>
2025-12-10 12:14:45 +0800

    1.0.13-SNAPSHOT: Move the test code to the test directory.

---
+++ src/test/java/net/mickarea/tools/maintests/TestMain1.java
@@ -0,0 +1,13 @@
+package net.mickarea.tools.maintests;
+
+import net.mickarea.tools.utils.Stdout;
+
+public class TestMain1 {
+
+	public static void main(String[] args) {
+		
+		Stdout.pl("测试");
+
+	}
+
+}
